53755666675c846cd74a2df9031347c6d8dbd7d4
saurav4622 <paruisourabh4622@gmail.com>
2024-05-01 17:57:05 +0530

    average file added

--- python/Homework2/test.py
+++
@@ -1,7 +0,0 @@
-import string
-count = 0
-str = "My name is Sourabh Parui I study in Techno India Hoohgly!!"
-for i in str:
-    if i in string.punctuation:
-        count += 1
-print("Total Puntuation:", count)
