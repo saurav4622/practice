fac6a227c1da1f30a8261b4c026008ee22449fc0
saurav4622 <paruisourabh4622@gmail.com>
2024-05-12 13:37:07 +0530

    recursion

--- python/new_assignments/recursion.py
+++ python/new_assignments/recursion.py
@@ -3,11 +3,20 @@
 # 2. Print a number in reverse form
 # 3. Convert a Decimal number to its equivalent binary number.
 def digit_sum(num):
-    new = 0
-    while num > 0:
-        a = num % 10
-        new = new + a
-        num = num // 10
-    return new
-number = int(input("Enter a number: "))
+    if num == 0:
+        return 0
+    else:
+        new = num % 10 + digit_sum(num // 10)
+        return new
+
+def reverse(num, new=0):
+    if num == 0:
+        return new
+    else:
+        last_digit = num % 10
+        new = new * 10 + last_digit
+        return reverse(num // 10, new)
+
+number = 8439
 print(digit_sum(number))
+print(reverse(number))
